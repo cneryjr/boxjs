1bf8406553867407609f64b573130ab986ca3b6b
Nery Jr <cneryjr@gmail.com>
2012-10-01 16:19:41 -0300

    Example: tutorial commited.

---
+++ examples/tutorial/WebContent/WEB-INF/boxjs/application.js
@@ -0,0 +1,25 @@
+function application(request) {
+	var contextPath = request.env.contextPath;
+	var uri = new String(request.env.request.getRequestURI());
+	var rest = uri.replace(contextPath, "").replace(/\/.*?\/(.*)/g, "$1");
+	var response = request.env.response;
+
+	if (rest.match(/.*?\.js$/g) != null) {
+		log.info("Running script " + rest);
+		global.queryString = request.queryString;
+		runScript(config.applicationRoot + rest);
+	} else {
+		response.setContentType("text/html");
+		response.write("<H1>boxJS is running!</H1>");
+	}
+	return {
+		status : 200,
+		headers : {
+			"content-type" : response.getContentType()
+		},
+		body : [ response.toString() ]
+	};
+}
+
+print("application.js loaded...........................................");
+
---
+++ examples/tutorial/WebContent/WEB-INF/boxjs/hello.js
@@ -0,0 +1,10 @@
+var response = global.response;
+var msg = " xxx ", x = {nome: "Pedro Paulo", idade: 8, cidade: "Uberlandia"};
+var qrys = parseParams(new String(global.queryString));
+
+
+print(JSON.stringify(qrys));
+
+response.write("<html> <head><title>abc</title></head> <body> <h1> Hello at "
+    + new Date() + " from " + global.request.getServletPath() 
+    + " </h1> <hr>" + JSON.stringify(x) + "<br>" + /*param.nome +*/ "<hr> </body> </html>");
