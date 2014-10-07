5daa8ab901df24c1b67dfc9a5d3203caef53d0d6
cneryjr <cneryjr@gmail.com>
2014-10-07 06:32:13 -0300

    Insert "session" in request parameter in processRequest method
    Insert "session" in request parameter in processRequest method

---
+++ examples/HelloWorld/src/java/softbox/boxjs/Utils.java
@@ -0,0 +1,11 @@
+package softbox.boxjs;
+
+import java.nio.file.Files;
+        
+/**
+ *
+ * @author nery
+ */
+public class Utils {
+
+}
--- examples/old/tutorial/WebContent/WEB-INF/boxjs/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	applicationRoot: "/",
-	entryPoint: "/application.js",
-	database: {
-		url: "jdbc:mysql://localhost:3306/sco",
-		driver: "com.mysql.jdbc.Driver",
-		user: "root"
-	}
-};
